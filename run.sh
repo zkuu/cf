408f3f6d06d684135ab7de5852125d3d731aed04
zkuu <47938258+zkuu@users.noreply.github.com>
2025-02-11 17:54:36 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-cp -f ip.txt ips-v4.txt
-cp -f ip.txt ips-v6.txt
+sed -i '1d' result.csv
+cut -d, --complement -f2,3,4,5 result.csv > temp.csv && mv temp.csv result.csv
+cp -f result.csv ips-v4.txt
+cp -f result.csv ips-v6.txt
 ./cfnat -colo 
