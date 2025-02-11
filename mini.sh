3f32ad0bc72b25163b0ec0b361e602f7f2e7d0fb
zkuu <47938258+zkuu@users.noreply.github.com>
2025-02-11 23:44:38 +0800

    Update mini.sh

--- mini.sh
+++ mini.sh
@@ -1,1 +1,1 @@
-sed -i '1d' result.csv && sed -i '101,3000d' result.csv && cut -d, --complement -f2,3,4,5 result.csv > temp.csv && mv temp.csv result.csv && cp -f result.csv /sdcard/name.txt
+sed -i '1d' result.csv && sed -i '51,3000d' result.csv && cut -d, --complement -f2,3,4,5 result.csv > temp.csv && mv temp.csv result.csv && cp -f result.csv /sdcard/name.txt
