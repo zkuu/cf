4d78b86def262f3fe0734fe0617da1917911a6b2
zkuu <47938258+zkuu@users.noreply.github.com>
2025-02-11 21:12:29 +0800

    Update mini.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 sed -i '1d' result.csv
 cut -d, --complement -f2,3,4,5 result.csv > temp.csv && mv temp.csv result.csv
-cp -f result.csv ips-v4.txt
-cp -f result.csv ips-v6.txt
-./cfnat -colo 
+sed -i '101,3000d' result.csv
+cp -f result.csv /sdcard/name.txt
